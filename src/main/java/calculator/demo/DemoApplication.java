131689cd0a3ae8e08976d9fdc7569c8167b0930e
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-01-08 19:54:42 +0100

    Fourth Commit

--- src/main/java/calculator/demo/DemoApplication.java
+++ src/main/java/calculator/demo/DemoApplication.java
@@ -1,13 +1,13 @@
 package calculator.demo;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 		Calculator calculator = new Calculator(5,1);
 		int result = calculator.addAtoB();
 		System.out.println(result);
